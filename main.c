0546217f2db71374d7e824298b4c5f4f305068e2
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-08-31 17:20:59 -0300

    feat: invaders

--- main.c
+++ main.c
@@ -8,6 +8,17 @@
 #define clear() printf("\033[2J")
 #define gotoxy(x, y) printf("\033[%d;%dH", (y), (x))
 
+#define MAX_INVADERS 55
+
+// 5 por 11
+
+typedef struct invader
+{
+    unsigned int x_position : 4;
+    unsigned int y_position : 4;
+
+} invader_t;
+
 // The function recieves a pointer to an empty termios struct
 // Saves the original terminal settings to original_terminal
 // Changes the current terminal canonity to non-canonical
@@ -39,16 +50,30 @@ int _kbhit()
     return keys_pressed;
 }
 
-void delay(unsigned duration)
+void init_invaders(invader_t *all_invaders)
 {
-    time_t start = time(NULL);
-    double end = duration;
-    time_t now = time(NULL);
-
-    do
+    for (int i = 0; i < 5; i++)
     {
-        now = time(NULL);
-    } while (difftime(now, start) < end);
+        for (int j = 0; j < 11; j++)
+        {
+            int invader_index = (i * 11) + j;
+
+            all_invaders[invader_index].x_position = j + 4;
+            all_invaders[invader_index].y_position = i + 1;
+        }
+    }
+}
+
+void print_invaders(invader_t *all_invaders)
+{
+    for (int i = 0; i < MAX_INVADERS; i++)
+    {
+        int x = all_invaders[i].x_position;
+        int y = all_invaders[i].y_position;
+
+        gotoxy(x, y);
+        printf("M");
+    }
 }
 
 int main(void)
@@ -57,13 +82,18 @@ int main(void)
 
     char input_character = 'a';
     int x_position = 5;
-    int y_position = 5;
+    int y_position = 9;
+
+    invader_t all_invaders[MAX_INVADERS];
+
+    if (initizalize_terminal(&original_terminal))
+        return 1;
 
     clear();
     system("tput civis");
 
-    if (initizalize_terminal(&original_terminal))
-        return 1;
+    init_invaders(all_invaders);
+    print_invaders(all_invaders);
 
     while (input_character != 'x')
     {
@@ -74,14 +104,20 @@ int main(void)
             switch (input_character)
             {
             case 'a':
-                gotoxy(x_position, y_position);
-                printf("%c", ' ');
-                x_position--;
+                if (x_position >= 2)
+                {
+                    gotoxy(x_position, y_position);
+                    printf("%c", ' ');
+                    x_position--;
+                }
                 break;
             case 'd':
-                gotoxy(x_position, y_position);
-                printf("%c", ' ');
-                x_position++;
+                if (x_position < 17)
+                {
+                    gotoxy(x_position, y_position);
+                    printf("%c", ' ');
+                    x_position++;
+                }
                 break;
             }
         }
