6825fe3cfabc047d5fcf15aa9f9005c5fd01c6cf
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-08-31 15:36:06 -0300

    funciton refactor

--- main.c
+++ main.c
@@ -2,17 +2,25 @@
 #include <termios.h>
 #include <unistd.h>
 
-int initizalize_terminal(struct termios *original_terminal, struct termios *new_terminal)
+// The function recieves a pointer to an empty termios struct
+// Saves the original terminal settings to original_terminal
+// Changes the current terminal canonity to non-canonical
+// returns 1 if the original settings could not be fetched.
+int initizalize_terminal(struct termios *original_terminal)
 {
+    struct termios new_terminal;
+
     if (tcgetattr(STDIN_FILENO, original_terminal) != 0)
         return 1;
 
-    if (tcgetattr(STDIN_FILENO, new_terminal) == 0)
+    if (tcgetattr(STDIN_FILENO, &new_terminal) == 0)
     {
-        new_terminal->c_lflag &= ~(ICANON);
-        new_terminal->c_lflag &= ~(ECHO);
-        tcsetattr(STDIN_FILENO, TCSAFLUSH, new_terminal);
+        new_terminal.c_lflag &= ~(ICANON);
+        new_terminal.c_lflag &= ~(ECHO);
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_terminal);
     }
+    else
+        return 1;
 
     return 0;
 }
@@ -20,11 +28,10 @@ int initizalize_terminal(struct termios *original_terminal, struct termios *new_
 int main(void)
 {
     struct termios original_terminal;
-    struct termios new_terminal;
 
     char input_character = 'a';
 
-    if (initizalize_terminal(&original_terminal, &new_terminal))
+    if (initizalize_terminal(&original_terminal))
         return 1;
 
     while (input_character != 'x')
